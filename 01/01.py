227caca2dc8dab0683d934d58ecf4945877d32d0
Matt <mantony42@gmail.com>
2019-12-02 23:34:52 -0500

    day 1

---
+++ 01/01.py
@@ -0,0 +1,26 @@
+def fuel(mass):
+    return mass//3-2
+
+def sum_part1(text):
+    total = 0
+    for line in text:
+        total += fuel(int(line))
+    return total
+
+def sum_part2(text):
+    total = 0
+    for line in text:
+        curr = int(line)
+        while curr > 0:
+            curr = fuel(curr)
+            total += max(0, curr)
+    return total
+
+if __name__ == "__main__":
+    with open('input.txt') as f:
+        part = 2
+
+        if part == 1:
+            print(sum_part1(f.readlines()))
+        else:
+            print(sum_part2(f.readlines()))
