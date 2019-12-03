1da48fa9a8edabc4a1164e5e45eedb374df88943
Matt <mantony42@gmail.com>
2019-12-02 21:50:13 -0500

    moved file reading outside of functions so it only happens once

--- 02/02.py
+++ 02/02.py
@@ -1,38 +1,42 @@
-def intcode_part1():
-    with open('input.txt') as f:
-        arr = [int(i) for i in f.read().split(',')]
-        arr[1] = 12
-        arr[2] = 2
-        for i in range(0, len(arr), 4):
-            if arr[i] == 99:
-                break
-            if arr[i] == 1:
-                arr[arr[i+3]] = arr[arr[i+2]] + arr[arr[i+1]]
-            if arr[i] == 2:
-                arr[arr[i+3]] = arr[arr[i+2]] * arr[arr[i+1]]
-        print(arr[0])
+def intcode_part1(text):
+    arr = [int(i) for i in text.split(',')]
+    arr[1] = 12
+    arr[2] = 2
+    for i in range(0, len(arr), 4):
+        if arr[i] == 99:
+            break
+        if arr[i] == 1:
+            arr[arr[i+3]] = arr[arr[i+2]] + arr[arr[i+1]]
+        if arr[i] == 2:
+            arr[arr[i+3]] = arr[arr[i+2]] * arr[arr[i+1]]
+    print(arr[0])
 
-def intcode_part2(x, y):
-    with open('input.txt') as f:
-        arr = [int(i) for i in f.read().split(',')]
-        arr[1] = x
-        arr[2] = y
-        for i in range(0, len(arr), 4):
-            if arr[i] == 99:
-                break
-            if arr[i] == 1:
-                arr[arr[i+3]] = arr[arr[i+2]] + arr[arr[i+1]]
-            if arr[i] == 2:
-                arr[arr[i+3]] = arr[arr[i+2]] * arr[arr[i+1]]
-        if arr[0] == 19690720:
-            print("x={} y={} 100*x+y={}".format(x, y, 100*x+y))
-            return True
+def intcode_part2(x, y, text):
+    arr = [int(i) for i in text.split(',')]
+    arr[1] = x
+    arr[2] = y
+    for i in range(0, len(arr), 4):
+        if arr[i] == 99:
+            break
+        if arr[i] == 1:
+            arr[arr[i+3]] = arr[arr[i+2]] + arr[arr[i+1]]
+        if arr[i] == 2:
+            arr[arr[i+3]] = arr[arr[i+2]] * arr[arr[i+1]]
+    if arr[0] == 19690720:
+        print("x={} y={} 100*x+y={}".format(x, y, 100*x+y))
+        return True
         
-def find_pair():        
+def find_pair(text):            
     for x in range(99):
         for y in range(99):
-            if intcode_part2(x, y):
+            if intcode_part2(x, y, text):
                 return
 
 if __name__ == "__main__":
-    find_pair()
+    with open('input.txt') as f:
+        part=2
+
+        if part==1:
+            intcode_part1(f.read())
+        else:
+            find_pair(f.read())
