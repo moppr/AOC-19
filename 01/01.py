a818c8842ee28e88cdcbb3c1928d5e669c533a35
Matt <mantony42@gmail.com>
2019-12-02 23:40:01 -0500

    cool guy way for part 1

--- 01/01.py
+++ 01/01.py
@@ -2,10 +2,7 @@ def fuel(mass):
     return mass//3-2
 
 def sum_part1(text):
-    total = 0
-    for line in text:
-        total += fuel(int(line))
-    return total
+    return sum([fuel(int(line)) for line in text])
 
 def sum_part2(text):
     total = 0
@@ -18,7 +15,7 @@ def sum_part2(text):
 
 if __name__ == "__main__":
     with open('input.txt') as f:
-        part = 2
+        part = 1
 
         if part == 1:
             print(sum_part1(f.readlines()))
